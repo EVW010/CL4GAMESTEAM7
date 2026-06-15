a633ce74f27446beadd51ca0c49a9521267145c5
Ansullen <coasterplanetcp@gmail.com>
2026-06-15 13:21:51 +0200

    Added level 1 map tilemap and made game.js go directly to level 1

--- src/js/game.js
+++ src/js/game.js
@@ -1,21 +1,23 @@
 import '../css/style.css'
 import { Actor, Engine, Vector, DisplayMode } from "excalibur"
 import { Resources, ResourceLoader } from './resources.js'
+import { MapLevel1 } from './maps/MapLevel1.js'
 
 export class Game extends Engine {
 
     constructor() {
-        super({ 
+        super({
             width: 1280,
             height: 720,
             maxFps: 60,
             displayMode: DisplayMode.FitScreen
          })
+        this.add('maplevel1', new MapLevel1())
         this.start(ResourceLoader).then(() => this.startGame())
     }
 
     startGame() {
-        console.log("start de game!")
+        this.goToScene('maplevel1')
     }
 }
 
