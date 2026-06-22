0dae078dfbc64f9c92a0e956c11ce4e214644119
Ansullen <coasterplanetcp@gmail.com>
2026-06-22 11:12:31 +0200

    I added the 4 levels

---
+++ src/js/maps/Level3/MapLevel3.js
@@ -0,0 +1,68 @@
+import { Actor, CollisionType, Vector } from 'excalibur'
+import wallTextureUrl from './assets/walls/wall3.png'
+import { MapEngine } from '../MapEngine.js'
+import { RenderObject } from '../../renderBase/renderbase.js'
+import { UI } from '../../ui.js'
+
+// . = floor, # = wall
+export const MAP = [
+    '####################',
+    '#..................#',
+    '#..#....#..#....#..#',
+    '#..................#',
+    '#....0.......0.....#',
+    '#..................#',
+    '#....0.......0.....#',
+    '#..................#',
+    '#..#....#..#....#..#',
+    '#..................#',
+    '####################',
+]
+
+export const isWallTile = (char) => char === '#'
+
+export class MapLevel3 extends MapEngine {
+    constructor(player) {
+        super(player)
+        this.map = MAP
+    }
+
+    isWallTile(char) {
+        return char === '#'
+    }
+
+    getMiniMapColor(tile) {
+        if (tile === '#') return 'rgb(150, 0, 150)'
+        return 'rgb(0, 0, 0)'
+    }
+
+    getTexture(tileType) {
+        return { img: this.wallImg, loaded: this.wallImgLoaded }
+    }
+
+    onMapSetup(engine) {
+        this.wallImg = new Image()
+        this.wallImg.src = wallTextureUrl
+        this.wallImgLoaded = false
+        this.wallImg.onload = () => { this.wallImgLoaded = true }
+
+        for (let y = 0; y < this.map.length; y++) {
+            for (let x = 0; x < this.map[y].length; x++) {
+                if (this.isWallTile(this.map[y][x])) {
+                    const wall = new Actor({ collisionType: CollisionType.Fixed })
+                    this.add(wall)
+                }
+
+                if (this.map[y][x] === '0') {
+                    const obj = new RenderObject(new Vector(x, y), 90, this.player)
+                    this.add(obj)
+                    const objt = new RenderObject(new Vector(x, y), 180, this.player)
+                    obj.vertical = 10
+                    this.add(objt)
+                }
+            }
+        }
+
+        this.add(new UI(this.player))
+    }
+}
